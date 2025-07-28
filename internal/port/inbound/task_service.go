134c39297fcc09729c8ba626af2727d0ddd6cfb0
Kidus Tessema <kidustessema91@gmail.com>
2025-07-28 09:03:05 +0300

    Update task_service.go

--- internal/port/inbound/task_service.go
+++ internal/port/inbound/task_service.go
@@ -1,1 +1,12 @@
+package inbound
+
+import "task-api/internal/domain"
+
+type TaskService interface {
+	Create(task *domain.Task) error
+	GetByID(id int64) (*domain.Task, error)
+	GetAll() ([]*domain.Task, error)
+	Update(task *domain.Task) error
+	Delete(id int64) error
+}
 
