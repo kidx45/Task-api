31711e9b74fe8e972e3aa6531c9dc642e911292a
Kidus Tessema <kidustessema91@gmail.com>
2025-07-28 08:53:01 +0300

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,36 @@
+package main
 
+import (
+	"database/sql"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	_"github.com/go-sql-driver/mysql"
+	"task-api/internal/adapter/inbound/http"
+	"task-api/internal/adapter/outbound"
+	"task-api/internal/application"
+	
+)
+
+func main() {
+	dsn := "root:manyahle1234$@@tcp(127.0.0.1:3306)/taskdb"
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("Failed to connect to DB: %v", err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatalf("DB unreachable: %v", err)
+	}
+
+	repo := persistence.NewMySQLRepo(db)
+	svc := service.NewTaskService(repo)
+
+	router := gin.Default()
+	http.NewTaskHandler(router, svc)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
