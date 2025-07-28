d8b78c9d9e781f743471f76078abccefcf496325
Kidus Tessema <kidustessema91@gmail.com>
2025-07-28 09:04:06 +0300

    Update task_repository.go

--- internal/port/outbound/task_repository.go
+++ internal/port/outbound/task_repository.go
@@ -1,1 +1,11 @@
+package outbound
 
+import "task-api/internal/domain"
+
+type TaskRepository interface {
+	Create(task *domain.Task) error
+	GetByID(id int64) (*domain.Task, error)
+	GetAll() ([]*domain.Task, error)
+	Update(task *domain.Task) error
+	Delete(id int64) error
+}
