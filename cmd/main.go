f004a37b2e3d9c89be7c9e62ae1999513526e8d2
Kidus Tessema <kidustessema91@gmail.com>
2025-07-29 14:13:33 +0300

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,36 +1,35 @@
 package main
 
 import (
-	"database/sql"
-	"log"
-
-	"github.com/gin-gonic/gin"
-	_"github.com/go-sql-driver/mysql"
-	"task-api/internal/adapter/inbound/http"
-	"task-api/internal/adapter/outbound"
-	"task-api/internal/application"
-	
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	httpy "task-api/internal/adapter/inbound/http"
+	"task-api/internal/adapter/outbound/persistence"
+	service "task-api/internal/application"
+	_ "github.com/go-sql-driver/mysql"
 )
 
+func getdata(s string, r *bufio.Reader) (string, error) {
+	fmt.Println(s)
+	i, err := r.ReadString('\n')
+	return strings.TrimSpace(i), err
+}
+
 func main() {
-	dsn := "root:manyahle1234$@@tcp(127.0.0.1:3306)/taskdb"
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		log.Fatalf("Failed to connect to DB: %v", err)
-	}
-	defer db.Close()
-
-	if err := db.Ping(); err != nil {
-		log.Fatalf("DB unreachable: %v", err)
-	}
-
-	repo := persistence.NewMySQLRepo(db)
-	svc := service.NewTaskService(repo)
-
-	router := gin.Default()
-	http.NewTaskHandler(router, svc)
-
-	if err := router.Run(":8080"); err != nil {
-		log.Fatalf("Failed to start server: %v", err)
+	user := bufio.NewReader(os.Stdin)
+	s, _ := getdata("For MySQL:1, For Postgres:2", user)
+	switch s {
+	case "2":
+		db := persistence.ConnectToPostgres()
+		database := persistence.CallPsql(db)
+		service := service.NewConnect(database)
+		httpy.Handler(service)
+	case "1":
+		db := persistence.ConnectToMysql()
+		database := persistence.CallMysql(db)
+		service := service.NewConnect(database)
+		httpy.Handler(service)
 	}
 }
